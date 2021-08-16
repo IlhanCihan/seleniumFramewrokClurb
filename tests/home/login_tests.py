eee8ae6d50e3f20242c0495a7bfa25112a576459
IlhanCihan <77418229+IlhanCihan@users.noreply.github.com>
2021-08-16 03:39:11 +0300

    Update login_tests.py

--- tests/home/login_tests.py
+++ tests/home/login_tests.py
@@ -2,18 +2,25 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from pages.home.login_page import LoginPage
 import unittest
+import pytest
+
 
 class LoginTests(unittest.TestCase):
+    baseURL = "http://automationpractice.com/index.php"
+    driver = webdriver.Chrome(executable_path="C:\\Users\\ASUS\\Desktop\\Selenium\\drivers\\chromedriver.exe")
+    driver.maximize_window()
+    driver.implicitly_wait(3)
+    driver.get(baseURL)
+    lp = LoginPage(driver)
+
+    @pytest.mark.run(order=2)
     def test_validLogin(self):
-        baseURL = "http://automationpractice.com/index.php"
-        driver = webdriver.Chrome(executable_path="C:\\Users\\ASUS\\Desktop\\Selenium\\drivers\\chromedriver.exe")
-        driver.maximize_window()
-        driver.implicitly_wait(3)
-        driver.get(baseURL)
+        self.driver.get(self.baseURL)
+        self.lp.login("automationtest12345@gmail.com", "test123")
+        result = self.lp.verifyLoginSuccessful()
 
-        lp = LoginPage(driver)
-        lp.login("automationtest12345@gmail.com", "test123")
-
+        assert result == True
+        self.driver.quit()
 
         # LoginLink = driver.find_element(By.LINK_TEXT, "Sign in")
         # LoginLink.click()
@@ -27,13 +34,25 @@ class LoginTests(unittest.TestCase):
         # loginButton = driver.find_element(By.ID, "SubmitLogin")
         # loginButton.click()
 
-        userName = driver.find_element(By.XPATH, "/html//header[@id='header']//nav//a[@title='View my customer account']/span[.='automation test']")
-        if userName is not None:
-            print("Successfully logged in")
-        else:
-            print("Failed to login")
+        # !! This is to confirm that we logged in should be diferent if logged in with different account
+        # userName = driver.find_element(By.XPATH, "/html//header[@id='header']//nav//a[@title='View my customer account']/span[.='automation test']")
+        # if userName is not None:
+        #     print("Successfully logged in")
+        # else:
+        #     print("Failed to login")
+
+    @pytest.mark.run(order=1)
+    def test_invalidLogin(self):
+        self.driver.get(self.baseURL)
+
+        self.lp.login("automationtest12345@gmail.com", "testWithInvalidPassword")
+        result = self.lp.verifyLoginFailed()
+
+        assert result == False
+
 
 # ff = LoginTests()
 # ff.test_validLogin()
+# ff.test_invalidLogin()
 if __name__ == "__main__":
     unittest.main()
